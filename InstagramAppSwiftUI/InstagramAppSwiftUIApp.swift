87c25ce05a4d4bfb875396cafc8d7462e7152559
omar thamri <omarthamri4@gmail.com>
2023-10-17 11:59:24 +0100

    first commit

---
+++ InstagramAppSwiftUI/InstagramAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramAppSwiftUIApp.swift
+//  InstagramAppSwiftUI
+//
+//  Created by omar thamri on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
